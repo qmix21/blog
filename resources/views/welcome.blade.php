1e72fe58c917dab3af9b5a47280210a63c5e00f4
James Mahon <james@ausgpl.com>
2018-08-24 11:59:22 +1000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+                <example-component></example-component>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
