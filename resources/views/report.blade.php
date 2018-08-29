e2d18725827126d0b0afe7ed5caed24322833e95
qmix21 <james@ausgpl.com>
2018-08-29 15:45:01 +1000

    test

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -43,6 +43,112 @@
     		</select>
     	</div>
     </div>
+
+<script src="https://raw.githubusercontent.com/nnnick/Chart.js/master/dist/Chart.bundle.js"></script>
+
+<script>
+
+    var year = ['2013','2014','2015', '2016'];
+
+    var data_click = <?php echo $click; ?>;
+
+    var data_viewer = <?php echo $viewer; ?>;
+
+
+    var barChartData = {
+
+        labels: year,
+
+        datasets: [{
+
+            label: 'Click',
+
+            backgroundColor: "rgba(220,220,220,0.5)",
+
+            data: data_click
+
+        }, {
+
+            label: 'View',
+
+            backgroundColor: "rgba(151,187,205,0.5)",
+
+            data: data_viewer
+
+        }]
+
+    };
+
+
+    window.onload = function() {
+
+        var ctx = document.getElementById("canvas").getContext("2d");
+
+        window.myBar = new Chart(ctx, {
+
+            type: 'bar',
+
+            data: barChartData,
+
+            options: {
+
+                elements: {
+
+                    rectangle: {
+
+                        borderWidth: 2,
+
+                        borderColor: 'rgb(0, 255, 0)',
+
+                        borderSkipped: 'bottom'
+
+                    }
+
+                },
+
+                responsive: true,
+
+                title: {
+
+                    display: true,
+
+                    text: 'Yearly Website Visitor'
+
+                }
+
+            }
+
+        });
+
+
+    };
+
+</script>
+
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-10 col-md-offset-1">
+
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+
+                    <canvas id="canvas" height="280" width="600"></canvas>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
     <!--================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
