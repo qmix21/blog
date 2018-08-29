8df80654586b04f419c1cf5439c37afba64d5c57
James Mahon <james@ausgpl.com>
2018-08-29 15:52:23 +1000

    test

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -44,11 +44,7 @@
     	</div>
     </div>
 
-<<<<<<< HEAD
-
-  
-=======
-<script src="https://cdnjs.ccloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js"></script>
+<script src="https://cdnjs.ccloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
 
 <script>
 
@@ -153,11 +149,9 @@
     </div>
 
 </div>
->>>>>>> e2d18725827126d0b0afe7ed5caed24322833e95
     <!--================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://raw.githubusercontent.com/nnnick/Chart.js/master/dist/Chart.bundle.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
 
   </body>
