5660d0f5caec73418309b0d2532ffd3a41d9d4d7
James Mahon <james@ausgpl.com>
2018-09-02 21:35:25 +1000

    test

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -146,6 +146,11 @@ console.log(year);
 
     });
 
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
     function getDates(startDate,stopDate)
     {
 
