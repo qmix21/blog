245569009adade8cdd3a4c7aa9980351f591329e
James Mahon <james@ausgpl.com>
2018-09-04 22:33:16 +1000

    tesdt

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -180,21 +180,12 @@ function addDays(date, days) {
 
 <div class="container">
 
-    <div class="row">
-
-        <div class="col-md-10 col-md-offset-1">
-
-            <div class="panel panel-default">
 
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
 
                     <canvas id="canvas" height="580" width="800"></canvas>
-
-                </div>
-
-            </div>
 
         </div>
 
