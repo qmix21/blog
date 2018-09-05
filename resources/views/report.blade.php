9beb93dc50508b3cb637e1d2cd7b2b3bfa373120
James Mahon <james@ausgpl.com>
2018-09-05 22:50:18 +1000

    test

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,115 +1,52 @@
-< !doctype html >
-    <
-    html lang = "en" >
+<!doctype html>
+    <html lang="en">
+        <head>
 
-    <
-    head >
-    <
-    meta charset = "utf-8" >
-    <
-    meta name = "viewport"
-content = "width=device-width, initial-scale=1, shrink-to-fit=no" >
-    <
-    meta name = "description"
-content = "" >
-    <
-    meta name = "author"
-content = "" >
-    <
-    link rel = "icon"
-href = "../../../../favicon.ico" >
+            <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <meta name="description" content="">
+                        <meta name="author" content="">
+                            <link rel="icon" href="../../../../favicon.ico">
 
-    <
-    title > Qmix21 Report < /title>
+                                <title> Qmix21 Report < /title> <!-- Bootstrap core CSS -->
+                                        <link href="{{asset("
+css / app.css ")}}" rel="stylesheet">
 
-    <!-- Bootstrap core CSS -->
-    <
-    link href = "{{asset("
-css / app.css ")}}"
-rel = "stylesheet" >
-
-    <!-- Custom styles for this template -->
-    <
-    script src = "https://code.jquery.com/jquery-latest.min.js" > < /script> <
-    script src = "https://d3js.org/d3.v4.min.js" > < /script> <
-    script src = "{{asset('js/barChart.js')}}" > < /script> <
-    /head>
-
-    <
-    body class = "text-center" >
-    <
-    h3 > Qmix21 Reporting Tool < /h3>
-
-    <
-    div class = "ctechs-container d-flex h-100 p-3 mx-auto flex-column" >
-    <
-    /header>
-
-    <
-    main role = "main"
-class = "inner ctechs" >
-    <
-    h1 class = "ctechs-heading" > Choose Between Two Dates to see Reports < /h1> <
-    /main> <
-    /div> <
-    div class = "container" >
-    <
-    div class = "col-sm-8" >
-    <
-    div class = "divider" > < /div> <
-    label > From: < /label> <
-    select style = "display: inline-block;"
-id = "sel1" >
-    @foreach($dates as $date) <
-    option > {
-        {
-            $date - > date
-        }
-    } < /option>
-@endforeach
-    <
-    /select> <
-    div class = "divider" > < /div> <
-    label > To: < /label> <
-    select style = "display: inline-block;"
-id = "sel2" >
-    @foreach($dates as $date) <
-    option > {
-        {
-            $date - > date
-        }
-    } < /option>
-@endforeach
-    <
-    /select> <
-    /div> <
-    /div> <
-    button onClick = "ShowChart()" > Show Chart < /button>
-
-    <
-    div class = "container" >
+                                            <!-- Custom styles for this template -->
+                                            <script src = "https://code.jquery.com/jquery-latest.min.js" ></script>
+                                            <script src = "https://d3js.org/d3.v4.min.js" ></script>
+                                            <script src = "{{asset('js/barChart.js')}}" ></script>
+                                                      </head>
+                                                      <body class="text-center">
+                                                            <h3> Qmix21 Reporting Tool </h3> <div class="ctechs-container d-flex h-100 p-3 mx-auto flex-column">
+                                                                    </header>
+                                                                    <main role="main" class="inner ctechs">
+                                                                        <h1 class="ctechs-heading"> Choose Between Two Dates to see Reports </h1> </main> </div> <div class="container">
+                                                                                <div class="col-sm-8">
+                                                                                    <div class="divider">
+                                                                                        </div> <label> From: </label> <select style="display: inline-block;" id="sel1">
+                                                                                                @foreach($dates as $date) <option> {
+                                                                                                    {
+                                                                                                    $date->date
+                                                                                                    }
+                                                                                                  } </option> @endforeach </select> <div class="divider">
+                                                                                                        </div> <label> To: </label> <select style="display: inline-block;" id="sel2">
+                                                                                                                @foreach($dates as $date) <option> {
+                                                                                                                    {
+                                                                                                                    $date->date
+                                                                                                                    }
+                                                                                                                    } </option> @endforeach </select> </div> </div> <button onClick="ShowChart()"> Show Chart </button> <div class="container">
 
 
-    <
-    div class = "panel-heading" > Dashboard < /div> <
-    script >
-    window.addEventListener('resize', function(event) {
-        $("#chart").width(window.innerWidth * 0.9);
-        $("#chart").height(window.innerHeight);
-    }); <
-/script> <
-div id = "chart" >
+                                                                                                                            <div class="panel-heading"> Dashboard </div> <script>
+                                                                                                                                    window.addEventListener('resize', function(event) {
+                                                                                                                                    $("#chart").width(window.innerWidth * 0.9);
+                                                                                                                                    $("#chart").height(window.innerHeight);
+                                                                                                                                    }); </script> <div id="chart">
 
 
-    <
-    /div>
-
-    <
-    /div>
-
-    <
-    /div> <
-    script >
+                                                                                                                                        </div> </div> </div>
+    <script>
 
 
     function ShowChart() {
@@ -134,13 +71,13 @@ div id = "chart" >
             var month = element.getMonth();
             var year = element.getFullYear().toString().slice(-2);
 
-            //  var names = <?php foreach($names as $name)
+            var names = <?php foreach($names as $name)
             {
                 $arr = [];
                 array_push($arr, $name - > name);
                 return $arr;
             } ? > ;
-            //console.log(names);
+            console.log(names);
         });
 
 
