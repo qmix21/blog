f74d7ca5c6eaed56e3f63e205a939ea88df2b666
James Mahon <james@ausgpl.com>
2018-09-11 21:45:57 +1000

    test

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -118,7 +118,7 @@
                                                                     }
 
                                                                   });
-
+                                                                  console.log(userData);
 
 
                                                                     }
